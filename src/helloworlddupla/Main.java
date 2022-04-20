866a199088de0831b61d88166afe7f0e49a51abb
soaresgabriel99 <oliveirasoaresgabriel21@gmail.com>
2022-04-20 20:05:31 -0300

    alteração do primeiro commit

--- src/helloworlddupla/Main.java
+++ src/helloworlddupla/Main.java
@@ -3,8 +3,8 @@ package helloworlddupla;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
-
+		System.out.println("Hello world!!");
+		System.out.println("Alteração do código!");
 	}
 
 }
