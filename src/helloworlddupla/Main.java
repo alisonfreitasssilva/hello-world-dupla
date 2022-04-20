cbd8d000956e5f0a480c5999e2f8995c727fb59d
Alison FSS <08220241@aluno.osorio.ifrs.edu.br>
2022-04-20 20:13:51 -0300

    Adicionado numero da sorte

--- src/helloworlddupla/Main.java
+++ src/helloworlddupla/Main.java
@@ -1,10 +1,16 @@
 package helloworlddupla;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
 		System.out.println("Alteração do código!");
+		
+		Random r = new Random();
+		
+		System.out.printf("Seu numero da sorte é: %d\n", r.nextInt() % 100);
 	}
 
 }
